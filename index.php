e05a2e38905bd12a746c66d36b6025c06db86186
LeiCraft <linusfisscher08@gmx.net>
2024-01-22 09:15:25 +0100

    update

--- index.php
+++ index.php
@@ -7,13 +7,17 @@
 
     <main>
         <div class="container">
-            <div class="total-status">
-                Alle Systeme Online
-            </div>
+            <div class="total-status text-center">Alle Systeme Online</div>
         </div>
     </main>
 
     <footer class="footer" id="footer"><?php include $_SERVER['DOCUMENT_ROOT'] . "/util/defaults/footer.php"; ?></footer>
+
+    <style>
+        .total-status {
+            padding: 12px 20px;
+        }
+    </style>
 
 </body>
 </html>
