f0a02ca8c8f9c6451612347c292b7fb2194d3572
LeiCraft <linusfisscher08@gmx.net>
2024-01-22 10:56:06 +0100

    update

--- index.php
+++ index.php
@@ -25,9 +25,7 @@
                                         <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
                                     </svg>
                                 </div>
-                                <div class="host-title-text">
-                                    <span>Main Server</span>
-                                </div>
+                                <div class="host-title-text h5 mb-0">Main Server</div>
                                 <div class="host-current-status ms-auto">Online</div>
                             </div>
                         </div>
@@ -44,9 +42,7 @@
                                         <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
                                     </svg>
                                 </div>
-                                <div class="host-title-text">
-                                    <span>Main Server</span>
-                                </div>
+                                <div class="host-title-text h5 mb-0">Main Server</div>
                                 <div class="host-current-status ms-auto">Online</div>
                             </div>
                         </div>
@@ -85,9 +81,6 @@
         }
         .host-title-text {
             padding-left: 10px;
-        }
-        .host-current-status {
-            float: right;
         }
     </style>
 
