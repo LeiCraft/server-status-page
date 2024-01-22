2f07646c2bd9d37e1a8ae987d5c6916098c13ef6
LeiCraft <linusfisscher08@gmx.net>
2024-01-22 10:47:07 +0100

    update

--- index.php
+++ index.php
@@ -18,11 +18,17 @@
                 <div class="main-host host-status-container">
                     <div class="host-status-inner-container">
                         <div class="host-title-container">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">
-                                <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
-                                <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
-                            </svg>
-                            <span class="host-title">Main Server</span>
+                            <div class="host-title">
+                                <div class="host-title-extend-img">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">
+                                        <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
+                                        <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
+                                    </svg>
+                                </div>
+                                <div class="host-title-text">
+                                    <span>Main Server</span>
+                                </div>
+                            </div>
                             <span class="host-current-status">Online</span>
                         </div>
                     </div>
@@ -31,11 +37,17 @@
                 <div class="main-host host-status-container">
                     <div class="host-status-inner-container">
                         <div class="host-title-container">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">
-                                <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
-                                <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
-                            </svg>
-                            <span class="host-title">Main Server</span>
+                            <div class="host-title">
+                                <div class="host-title-extend-img">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">
+                                        <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
+                                        <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
+                                    </svg>
+                                </div>
+                                <div class="host-title-text">
+                                    <span>Main Server</span>
+                                </div>
+                            </div>
                             <span class="host-current-status">Online</span>
                         </div>
                     </div>
@@ -63,9 +75,18 @@
         .host-status-inner-container {
             margin: 10px 15px;
         }
-        .host-title-container {
+        .host-title {
             display: flex;
             align-items: center;
+        }
+        .host-title-extend-img {
+            display: flex;
+            align-items: center
+        }
+        .host-title-text {
+            display: flex;
+            align-items: center
+            padding-left: 10px;
         }
         .host-current-status {
             float: right;
