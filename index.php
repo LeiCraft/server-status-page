c9b086ce646741da77b8c1a1d2f7d6c9d0035706
LeiCraft <linusfisscher08@gmx.net>
2024-01-23 08:26:33 +0100

    update

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
                             <div class="collapse" id="main-host-collapse">
 
                                 <div class="chart-container mt-4">
-                                    <svg id="chart-svg" xmlns="http://www.w3.org/2000/svg"></svg>
+                                    <svg id="chart-svg" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none"></svg>
                                 </div>
 
                                 <div class="service-status-section d-flex flex-column mt-2">
@@ -42,7 +42,7 @@
                                                     <div class="host-title-text h5 mb-0">Main Server</div>
                                                     <div class="host-current-status ms-auto">Online</div>
                                                     <div class="host-title-extend-img">
-                                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-down host-title-arrow-icon" viewBox="0 0 16 16" preserveAspectRatio="none">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-down host-title-arrow-icon" viewBox="0 0 16 16">
                                                             <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z" stroke="currentColor" stroke-width="2"></path>
                                                         </svg>
                                                     </div>
