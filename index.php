2ec8cd5819ba3fd19171c03cf4029a09d0f0bbb4
LeiCraft <linusfisscher08@gmx.net>
2024-01-23 08:25:28 +0100

    update

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
                             </div>
                             <div class="collapse" id="main-host-collapse">
 
-                                <div id="chart-container" class="mt-4">
+                                <div class="chart-container mt-4">
                                     <svg id="chart-svg" xmlns="http://www.w3.org/2000/svg"></svg>
                                 </div>
 
@@ -42,7 +42,7 @@
                                                     <div class="host-title-text h5 mb-0">Main Server</div>
                                                     <div class="host-current-status ms-auto">Online</div>
                                                     <div class="host-title-extend-img">
-                                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-down host-title-arrow-icon" viewBox="0 0 16 16">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-down host-title-arrow-icon" viewBox="0 0 16 16" preserveAspectRatio="none">
                                                             <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z" stroke="currentColor" stroke-width="2"></path>
                                                         </svg>
                                                     </div>
@@ -107,8 +107,8 @@
             margin: 15px 5px 15px 15px;
         }
 
-        #chart-container {
-            overflow-y: scroll;
+        .chart-container {
+            width: 100%;
         }
 
         #chart-svg {
