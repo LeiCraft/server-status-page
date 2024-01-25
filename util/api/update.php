fc6276ad703679795539310a8152123d51d1cf1c
LeiCraft <linusfisscher08@gmx.net>
2024-01-25 10:35:52 +0100

    update

--- util/api/update.php
+++ util/api/update.php
@@ -7,15 +7,15 @@ use Spatie\Async\Pool;
 $hosts = require_once $_SERVER['DOCUMENT_ROOT'] . "/util/config/hosts.php";
 
 
-/*function runUpdate() {
+function runUpdate() {
 
     fetchCurrentStaus();
     return "success";
 
-}*/
+}
 
 
-function runUpdate() {
+function fetchCurrentStaus() {
     $results = [];
     
     // Create a new Pool
