12072c8d746f2d93c832f1d43ca55ffb07957b2b
LeiCraft <linusfisscher08@gmx.net>
2024-01-26 11:44:20 +0100

    update

--- util/api/mysql.php
+++ util/api/mysql.php
@@ -1,5 +1,9 @@
 <?php
 
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
     class DatabaseConnection {
         private static $instance; // The single instance
         private $connection;
